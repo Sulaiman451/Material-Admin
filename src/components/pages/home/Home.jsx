068190ffa4086c1bd3ffcf3b8d32d7444e17603b
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-08-05 18:24:01 +0100

    User information components

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from '../../charts/Chart'
 import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
 import './Home.css'
+import { userData } from "../../../dummyData";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active Users" />
     </div>
   )
 }
---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active Users": 4000,
+  },
+  {
+    name: "Feb",
+    "Active Users": 5000,
+  },
+  {
+    name: "Mar",
+    "Active Users": 6000,
+  },
+  {
+    name: "Apr",
+    "Active Users": 5000,
+  },
+  {
+    name: "May",
+    "Active Users": 6000,
+  },
+  {
+    name: "Jun",
+    "Active Users": 7000,
+  },
+  {
+    name: "Jul",
+    "Active Users": 8000,
+  },
+  {
+    name: "Aug",
+    "Active Users": 8000,
+  },
+  {
+    name: "Sep",
+    "Active Users": 7000,
+  },
+  {
+    name: "Oct",
+    "Active Users": 8000,
+  },
+  {
+    name: "Nov",
+    "Active Users": 9000,
+  },
+  {
+    name: "Dec",
+    "Active Users": 8000,
+  },
+];
